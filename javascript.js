53d81037323912509b7f9337e3ceb0734968a8ac
sherleysosa <sherleysosa2189@gmail.com>
2025-04-11 13:20:08 -0400

    Modified index.html, javascrip.js and styles.css -Added a new button class and id in the index.html file -Added style to the button and change the dimentions of container afer modifying the javascript file -Added a event that when pressed the button, the user can add the size of the squares -Added the function of clearGrid and modified the createGrid function to create a new grid when reseting it -Added the new dimentions of the square sizes with the new number

--- javascript.js
+++ javascript.js
@@ -1,16 +1,49 @@
-document.addEventListener ('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const container = document.getElementById('container');
-    createGrid(16);
-
-    function createGrid(size) {
-        const totalSquares = size * size;
-      
-        for (let i = 0; i < totalSquares; i++) {
-          const square = document.createElement('div');
-          square.classList.add('square');
-          container.appendChild(square);
-        }
+    const button = document.getElementById('button');
+  
+    const containerSize = 960;
+  
+    createGrid(16); 
+  
+    
+    button.addEventListener('click', function () {
+      let size = prompt("Enter number of squares per side (max 100):");
+      size = parseInt(size);
+  
+      if (size = NaN|| size <= 0 || size > 100) {
+        alert("Please enter a valid number between 1 and 100.");
+        return;
       }
-})
+  
+      clearGrid();
+      createGrid(size);
+    });
+  
+    function createGrid(squaresPerSide) {
+      const squareSize = containerSize / squaresPerSide;
+  
+      for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+  
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+  
+        square.addEventListener('mouseover', function () {
+          square.style.backgroundColor = 'orange';
+        });
+  
+        container.appendChild(square);
+      }
+    }
+  
+    function clearGrid() {
+      container.innerHTML = '';
+    }
+  });
+  
+  
 
-console.log (createGrid(16));
+
+
